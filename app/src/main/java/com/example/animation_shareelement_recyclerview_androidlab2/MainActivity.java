8274575bc040394b1e0ffc2a33dc6ea188c0d804
Vuong <vuong@11.com>
2021-09-24 17:55:40 +0700

    animation-shareElement

---
+++ app/src/main/java/com/example/animation_shareelement_recyclerview_androidlab2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.animation_shareelement_recyclerview_androidlab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
